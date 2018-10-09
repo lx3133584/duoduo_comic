d9967f5590a28e2c1f5ba2a8b088e5ba6d097b29
liangxu <liangxu@nle-tech.com>
2018-10-09 11:38:57 +0800

    add react-native-debugger

--- app/store/index.js
+++ app/store/index.js
@@ -3,6 +3,8 @@ import { createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import immutableTransform from 'redux-persist-transform-immutable';
 import { AsyncStorage } from 'react-native';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import Immutable from 'immutable';
 import middleware, { epicMiddleware } from './middleware';
 import rootReducer from './reducer';
 import rootEpic from './epic';
@@ -16,7 +18,13 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const store = createStore(persistedReducer, applyMiddleware(...middleware));
+const composeEnhancers = composeWithDevTools({
+  serialize: {
+    immutable: Immutable,
+  },
+});
+
+const store = createStore(persistedReducer, composeEnhancers(applyMiddleware(...middleware)));
 
 epicMiddleware.run(rootEpic);
 
--- app/store/middleware.js
+++ app/store/middleware.js
@@ -1,26 +1,26 @@
 import promiseMiddleware from 'redux-promise-middleware';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import { createEpicMiddleware } from 'redux-observable';
 
 export const epicMiddleware = createEpicMiddleware();
 
 const middleware = [promiseMiddleware(), epicMiddleware];
 
-if (__DEV__) {
-  const logger = createLogger({
-    stateTransformer: (state) => {
-      const newState = {};
-      for (const k in state) {
-        const item = state[k];
-        if (item.toJS) {
-          newState[k] = item.toJS();
-        } else {
-          newState[k] = item;
-        }
-      }
-      return newState;
-    },
-  });
-  middleware.push(logger);
-}
+// if (__DEV__) {
+//   const logger = createLogger({
+//     stateTransformer: (state) => {
+//       const newState = {};
+//       for (const k in state) {
+//         const item = state[k];
+//         if (item.toJS) {
+//           newState[k] = item.toJS();
+//         } else {
+//           newState[k] = item;
+//         }
+//       }
+//       return newState;
+//     },
+//   });
+//   middleware.push(logger);
+// }
 export default middleware;
