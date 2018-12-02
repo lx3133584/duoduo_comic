e75639977000880d469082cfc05826c6c010e447
liangxu <liangxu@nle-tech.com>
2018-12-02 23:08:57 +0800

    图片加载失败可以点击重试

--- app/modules/comic/comic_content/content_list_item/component.js
+++ app/modules/comic/comic_content/content_list_item/component.js
@@ -2,12 +2,23 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Platform } from 'react-native';
 import FastImage from 'react-native-fast-image';
+import Button from 'apsl-react-native-button';
 import { createImageProgress } from 'react-native-image-progress';
 import { ImgPlaceholder } from '@/comic/comic_content';
 import { getImgHeight } from 'utils';
 
 const Image = createImageProgress(FastImage);
-
+const buttonStyle = {
+  backgroundColor: '#999',
+  borderColor: '#eee',
+  width: 100,
+  height: 40,
+  alignSelf: 'center',
+};
+const textStyle = {
+  color: '#eee',
+  fontSize: 16,
+};
 class ContentListItem extends PureComponent {
   static propTypes = {
     url: PropTypes.string.isRequired,
@@ -24,15 +35,22 @@ class ContentListItem extends PureComponent {
     path: '',
   }
 
+  state = {
+    reload_key: 1,
+  }
+
+  reloadImage = () => this.setState(prev => ({ reload_key: prev.reload_key + 1 }))
+
   render() {
     const {
       url, path, index, size, width,
     } = this.props;
+    const { reload_key } = this.state;
     const uri = path
       ? Platform.OS === 'android'
         ? `file://${path}`
         : path
-      : url;
+      : `${url}?reload=${reload_key}`;
     const style = {
       width,
       height: getImgHeight(size, width),
@@ -56,7 +74,15 @@ class ContentListItem extends PureComponent {
             style={style}
             title={`${index}`}
             subTitle="加载失败"
-          />
+          >
+            <Button
+              style={buttonStyle}
+              textStyle={textStyle}
+              onPress={this.reloadImage}
+            >
+              重新加载
+            </Button>
+          </ImgPlaceholder>
         )}
       />
     );
--- app/modules/comic/comic_content/img_placeholder/component.js
+++ app/modules/comic/comic_content/img_placeholder/component.js
@@ -7,6 +7,11 @@ const ContainStyled = styled.View`
   justify-content: center;
   align-items: center;
 `;
+const ContentStyled = styled.View`
+  justify-content: center;
+  align-items: center;
+  margin-top: 8px;
+`;
 const TitleStyled = styled.Text`
   font-size: 32px;
   color: #eee;
@@ -15,24 +20,31 @@ const SubTitleStyled = styled.Text`
   font-size: 18px;
   color: #eee;
 `;
-function ImagePlaceholder({ title, subTitle, ...otherProps }) {
+function ImagePlaceholder({
+  title, subTitle, children, ...rest
+}) {
   return (
-    <ContainStyled {...otherProps}>
+    <ContainStyled {...rest}>
       <TitleStyled>
         {title}
       </TitleStyled>
       <SubTitleStyled>
         {subTitle}
       </SubTitleStyled>
+      <ContentStyled>
+        {children}
+      </ContentStyled>
     </ContainStyled>
   );
 }
 ImagePlaceholder.propTypes = {
   title: PropTypes.string,
   subTitle: PropTypes.string,
+  children: PropTypes.element,
 };
 ImagePlaceholder.defaultProps = {
   title: '',
   subTitle: '',
+  children: null,
 };
 export default ImagePlaceholder;
