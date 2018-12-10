f603c92e8640b85ec548229ced37b0de22bcd814
梁旭 <liangxu@liangxudeMacBook-Pro.local>
2018-12-10 20:46:16 +0800

    设置中心添加阅读设置

--- app/modules/user/setting_center/Main/component.js
+++ app/modules/user/setting_center/Main/component.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import ActionSheet from 'react-native-actionsheet';
 import Toast from 'react-native-root-toast';
 import * as CacheManager from 'react-native-http-cache2';
 import { Modal } from '@';
@@ -10,10 +11,43 @@ const ContainStyled = styled.View`
   background: #fff;
 `;
 
+const orientationData = [
+  {
+    text: '竖屏',
+    value: 'vertical',
+  },
+  {
+    text: '横屏',
+    value: 'horizon',
+  },
+];
+const modeData = [
+  {
+    text: '滚动模式',
+    value: 'scroll',
+  },
+  {
+    text: '翻页模式',
+    value: 'page_turning',
+  },
+];
+
+const orientationTextMap = orientationData.reduce((map, item) => ({ ...map, [item.value]: item.text }), {});
+const modeTextMap = modeData.reduce((map, item) => ({ ...map, [item.value]: item.text }), {});
+
+const orientationOptions = orientationData.map(item => item.text);
+orientationOptions.push('取消');
+const modeOptions = modeData.map(item => item.text);
+modeOptions.push('取消');
+
 class MainComponent extends PureComponent {
   static propTypes = {
     isLogin: PropTypes.bool.isRequired,
+    mode: PropTypes.string.isRequired,
+    orientation: PropTypes.string.isRequired,
     logout: PropTypes.func.isRequired,
+    switchOrientation: PropTypes.func.isRequired,
+    switchReadingMode: PropTypes.func.isRequired,
   };
 
   state = {
@@ -59,13 +93,45 @@ class MainComponent extends PureComponent {
     this.setState({ isVisible: false });
   };
 
+  switchOrientation = (index) => {
+    if (index >= 2) return;
+    const { switchOrientation, switchReadingMode } = this.props;
+    const { value } = orientationData[index] || {};
+    if (!value) return;
+    if (value === 'horizon') {
+      switchReadingMode('scroll');
+    }
+    switchOrientation(value);
+  }
+
+  switchMode = (index) => {
+    if (index >= 2) return;
+    const { switchReadingMode } = this.props;
+    const { value } = modeData[index] || {};
+    if (!value) return;
+    switchReadingMode(value);
+  }
+
   render() {
     const { isVisible, cacheSize } = this.state;
-    const { isLogin } = this.props;
+    const { isLogin, orientation, mode } = this.props;
     return (
       <ContainStyled>
         <ListItem
-          key="cache"
+          title="屏幕方向"
+          rightTitle={orientationTextMap[orientation]}
+          onPress={this.orientationActionSheet && (() => this.orientationActionSheet.show())}
+        />
+        {
+          orientation === 'vertical' && (
+            <ListItem
+              title="阅读模式"
+              rightTitle={modeTextMap[mode]}
+              onPress={this.modeActionSheet && (() => this.modeActionSheet.show())}
+            />
+          )
+        }
+        <ListItem
           title="清除缓存"
           rightTitle={`${(cacheSize / 1048576).toFixed(2)}MB`}
           onPress={this.clearCache}
@@ -73,7 +139,6 @@ class MainComponent extends PureComponent {
         {isLogin && (
           <ListItem
             chevron
-            key="logout"
             title="退出登录"
             onPress={this.logout}
           />
@@ -86,6 +151,20 @@ class MainComponent extends PureComponent {
 
           是否确认退出登录？
         </Modal>
+        <ActionSheet
+          ref={o => this.orientationActionSheet = o}
+          title="选择上传方式"
+          options={orientationOptions}
+          cancelButtonIndex={2}
+          onPress={this.switchOrientation}
+        />
+        <ActionSheet
+          ref={o => this.modeActionSheet = o}
+          title="选择上传方式"
+          options={modeOptions}
+          cancelButtonIndex={2}
+          onPress={this.switchMode}
+        />
       </ContainStyled>
     );
   }
--- app/modules/user/setting_center/Main/container.js
+++ app/modules/user/setting_center/Main/container.js
@@ -1,15 +1,23 @@
 import { connect } from 'react-redux';
 import Component from './component';
 import { logoutAction } from '../actions';
+import { configActions } from '@';
 
 const mapStateToProps = state => ({
   isLogin: !!state.user.get('info').size,
   orientation: state.config.get('orientation'),
+  mode: state.config.get('mode'),
 });
 
 const mapDispatchToProps = dispatch => ({
   logout() {
     return dispatch(logoutAction());
+  },
+  switchOrientation(params) {
+    return dispatch(configActions.switchOrientation(params));
+  },
+  switchReadingMode(params) {
+    return dispatch(configActions.switchReadingMode(params));
   },
 });
 
